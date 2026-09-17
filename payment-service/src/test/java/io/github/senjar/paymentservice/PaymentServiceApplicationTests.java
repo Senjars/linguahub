4a2c7a5965fbb3239baf0f8f8555464f527ef646
Senjars <jaroslawsendecki1@gmail.com>
2026-09-17 14:35:54 +0200

    feat(api-gateway): add API gateway with service discovery routing

---
+++ payment-service/src/test/java/io/github/senjar/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.senjar.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
